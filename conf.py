ce1144c86d313c6db1cd3acd63e4f2a3aa824da8
Cangalala <103964892+Cangalala@users.noreply.github.com>
2022-06-17 10:22:12 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,4 +1,4 @@
 api_hash = "2618ec2cdeb1b513eabf02f83f5a1553"
 api_id = 15745491
 bot_token = "5458792094:AAFm6hT_OjZGHfeoS8t6BJ4GoFRnhz4oSsk"
-bot_admin_group = -1001766577678
+bot_admin_group = -1449038487
